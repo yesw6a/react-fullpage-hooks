3233eea8181be4caea2cc637fcc73798431e69d8
w6a <583783099@qq.com>
2020-04-10 21:00:07 +0800

    fix: 修复example无法正确自动打开的问题

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -7,31 +7,31 @@ const devConfig = {
   entry: path.join(__dirname, "../example/src/index.js"), // 项目入口，处理资源文件的依赖关系
   output: {
     path: path.join(__dirname, "../example/src/"),
-    filename: "bundle.js" // 使用webpack-dev-sevrer启动开发服务时，并不会实际在`src`目录下生成bundle.js，打包好的文件是在内存中的，但并不影响我们使用。
+    filename: "bundle.js", // 使用webpack-dev-sevrer启动开发服务时，并不会实际在`src`目录下生成bundle.js，打包好的文件是在内存中的，但并不影响我们使用。
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        loader: ["style-loader", "css-loader?modules", "sass-loader"]
+        loader: ["style-loader", "css-loader?modules", "sass-loader"],
       },
       {
         test: /\.css$/,
         exclude: /\.min\.css$/,
-        loader: ["style-loader", "css-loader?modules", "sass-loader"]
+        loader: ["style-loader", "css-loader?modules", "sass-loader"],
       },
       {
         test: /\.min\.css$/,
-        loader: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
+        loader: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
   devServer: {
-    contentBase: path.join(__dirname, "../example/src/"),
+    contentBase: path.join(__dirname, "../example/src"),
     compress: true,
     host: "127.0.0.1", // webpack-dev-server启动时要指定ip，不能直接通过localhost启动，不指定会报错
     port: 3000, // 启动端口为 3000 的服务
-    open: true // 自动打开浏览器
-  }
+    open: true, // 自动打开浏览器
+  },
 };
 module.exports = merge(devConfig, baseConfig); // 将baseConfig和devConfig合并为一个配置
